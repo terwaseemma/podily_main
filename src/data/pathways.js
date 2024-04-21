c5c842f448bef1abf7d7242a54fd13295e957bf4
emmanuel <emmanuelterwase1@gmail.com>
2024-04-21 16:59:12 +0000

    new updates

--- src/data/pathways.js
+++ src/data/pathways.js
@@ -1,43 +1,37 @@
-import pathway1 from '../../assets/pathway1.png'
-import pathway2 from '../../assets/pathway2.png'
-import pathway3 from '../../assets/pathway3.png'
-import pathway4 from '../../assets/pathway4.png'
-import pathway5 from '../../assets/pathway5.png'
-
 const pathways = [
     {
       id: 1,
       title: "Student",
       description: "Are you tired of struggling to finish your projects on time? We have the right solution for you...",
-      image: pathway1,
+    //   image: pathway1,
       rating: 10
     },
     {
       id: 2,
       title: "Student",
       description: "Are you tired of struggling to finish your projects on time? We have the right solution for you...",
-      image: pathway2,
+    //   image: pathway2,
       rating: 10
     },
     {
       id: 3,
       title: "Student",
       description: "Are you tired of struggling to finish your projects on time? We have the right solution for you...",
-      image: pathway3,
+    //   image: pathway3,
       rating: 9
     },
     {
       id: 4,
       title: "Student",
       description: "Are you tired of struggling to finish your projects on time? We have the right solution for you...",
-      image: pathway4,
+    //   image: pathway4,
       rating: 8
     },
     {
       id: 5,
       title: "Student",
       description: "Are you tired of struggling to finish your projects on time? We have the right solution for you...",
-      image: pathway5,
+    //   image: pathway5,
       rating: 10
     },
   ]
